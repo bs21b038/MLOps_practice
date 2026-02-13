a7515713b825e72ccc87c3deceb8b94d4f35bf24
Keerthana <tripurarinagakeerthana@gmail.com>
2026-02-13 20:12:15 +0530

    updated fastapi.py by including /health endpoint

--- fastapi.py
+++ fastapi.py
@@ -2,6 +2,12 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
 def home():
-    return{"message": "Hello Keerthana"}
+    return {"message": "Hello Keerthana"}
+
+
+@app.get("/health")
+def health():
+    return {"status": "ok"}
